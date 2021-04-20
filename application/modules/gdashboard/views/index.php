704b8748d4b42c575ae4f9a787cf31a98b55bc6b
tasminamin <tasmienamien@gmail.com>
2021-04-20 16:47:00 +0700

    index dashboard

--- application/modules/gdashboard/views/index.php
+++ application/modules/gdashboard/views/index.php
@@ -2,11 +2,10 @@
  <!-- breadcrumb -->
  <div class="breadcrumb-header justify-content-between">
      <div>
-         <h4 class="content-title mb-2">Hi, Ini Catatan Pengaduan</h4>
+         <h4 class="content-title mb-2"></h4>
          <nav aria-label="breadcrumb">
              <ol class="breadcrumb">
-                 <li class="breadcrumb-item"><a href="#">Aksi</a></li>
-                 <li class="breadcrumb-item active" aria-current="page"> Pengaduan</li>
+                <BR>
              </ol>
          </nav>
      </div>
@@ -70,48 +69,160 @@
  <?php $token = date("His") ?>
 
 
- <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
-     <div class="card">
-         <div class="info-box bg-pink hover-expand-effect">
-             <div class="icon">
-                 <i class="material-icons">playlist_add_check</i>
-             </div>
-             <div class="content">
-                 <div class="text">TOTAL KELAS MENGAJAR</div>
-                 <div class="number count-to" data-from="0" data-to="125" data-speed="15" data-fresh-interval="20"><?php echo $this->m_guru->totalKelasMengajar($this->m_guru->idu()) ?></div>
-             </div>
-         </div>
+ <div class="main-content-body">
+					<div class="row row-sm">
+						<div class="col-xl-4 col-lg-6 col-md-6 col-sm-12">
+							<div class="card overflow-hidden project-card">
+								<div class="card-body">
+									<div class="d-flex">
+										<div class="my-auto">
+											<svg enable-background="new 0 0 469.682 469.682" version="1.1"  class="mr-4 ht-60 wd-60 my-auto primary" viewBox="0 0 469.68 469.68" xml:space="preserve" xmlns="http://www.w3.org/2000/svg">
+                                            path d="m347.97 57.503h-39.706v-17.763c0-5.747-6.269-8.359-12.016-8.359h-30.824c-7.314-20.898-25.6-31.347-46.498-31.347-20.668-0.777-39.467 11.896-46.498 31.347h-30.302c-5.747 0-11.494 2.612-11.494 8.359v17.763h-39.707c-23.53 0.251-42.78 18.813-43.886 42.318v299.36c0 22.988 20.898 39.706 43.886 39.706h257.04c22.988 0 43.886-16.718 43.886-39.706v-299.36c-1.106-23.506-20.356-42.068-43.886-42.319zm-196.44-5.224h28.735c5.016-0.612 9.045-4.428 9.927-9.404 3.094-13.474 14.915-23.146 28.735-23.51 13.692 0.415 25.335 10.117 28.212 23.51 0.937 5.148 5.232 9.013 10.449 9.404h29.78v41.796h-135.84v-41.796zm219.43 346.91c0 11.494-11.494 18.808-22.988 18.808h-257.04c-11.494 0-22.988-7.314-22.988-18.808v-299.36c1.066-11.964 10.978-21.201 22.988-21.42h39.706v26.645c0.552 5.854 5.622 10.233 11.494 9.927h154.12c5.98 0.327 11.209-3.992 12.016-9.927v-26.646h39.706c12.009 0.22 21.922 9.456 22.988 21.42v299.36z"/>
+												<path d="m179.22 233.57c-3.919-4.131-10.425-4.364-14.629-0.522l-33.437 31.869-14.106-14.629c-3.919-4.131-10.425-4.363-14.629-0.522-4.047 4.24-4.047 10.911 0 15.151l21.42 21.943c1.854 2.076 4.532 3.224 7.314 3.135 2.756-0.039 5.385-1.166 7.314-3.135l40.751-38.661c4.04-3.706 4.31-9.986 0.603-14.025-0.19-0.211-0.391-0.412-0.601-0.604z"/>
+												<path d="m329.16 256.03h-120.16c-5.771 0-10.449 4.678-10.449 10.449s4.678 10.449 10.449 10.449h120.16c5.771 0 10.449-4.678 10.449-10.449s-4.678-10.449-10.449-10.449z"/>
+												<path d="m179.22 149.98c-3.919-4.131-10.425-4.364-14.629-0.522l-33.437 31.869-14.106-14.629c-3.919-4.131-10.425-4.364-14.629-0.522-4.047 4.24-4.047 10.911 0 15.151l21.42 21.943c1.854 2.076 4.532 3.224 7.314 3.135 2.756-0.039 5.385-1.166 7.314-3.135l40.751-38.661c4.04-3.706 4.31-9.986 0.603-14.025-0.19-0.211-0.391-0.412-0.601-0.604z"/>
+												<path d="m329.16 172.44h-120.16c-5.771 0-10.449 4.678-10.449 10.449s4.678 10.449 10.449 10.449h120.16c5.771 0 10.449-4.678 10.449-10.449s-4.678-10.449-10.449-10.449z"/>
+												<path d="m179.22 317.16c-3.919-4.131-10.425-4.363-14.629-0.522l-33.437 31.869-14.106-14.629c-3.919-4.131-10.425-4.363-14.629-0.522-4.047 4.24-4.047 10.911 0 15.151l21.42 21.943c1.854 2.076 4.532 3.224 7.314 3.135 2.756-0.039 5.385-1.166 7.314-3.135l40.751-38.661c4.04-3.706 4.31-9.986 0.603-14.025-0.19-0.21-0.391-0.411-0.601-0.604z"/>
+												<path d="m329.16 339.63h-120.16c-5.771 0-10.449 4.678-10.449 10.449s4.678 10.449 10.449 10.449h120.16c5.771 0 10.449-4.678 10.449-10.449s-4.678-10.449-10.449-10.449z"/>
+											
+											</svg>
+										</div>
+										<div class="project-content">
+											<h5>TOTAL KELAS MENGAJAR</h5>
+											<ul>
+												<li>
+                                               <h3 class=" text-primary"> <?php echo $this->m_guru->totalKelasMengajar($this->m_guru->idu()) ?></h3>
+													
+												</li>
 
-         <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
-             <div class="info-box bg-cyan hover-expand-effect">
-                 <div class="icon">
-                     <i class="material-icons">alarm</i>
-                 </div>
-                 <div class="content">
-                     <div class="text">JAM MENGAJAR</div>
-                     <div class="number count-to" data-from="0" data-to="257" data-speed="1000" data-fresh-interval="20"><?php echo $this->m_guru->totalJamMengajarPerMinggu($this->m_guru->idu()) ?> Jam / Minggu</div>
-                 </div>
-             </div>
-         </div>
-         <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
-             <div class="info-box bg-light-green hover-expand-effect">
-                 <div class="icon">
-                     <i class="material-icons">alarm_on</i>
-                 </div>
-                 <div class="content">
-                     <div class="text">TOTAL JAM MENGAJAR</div>
-                     <div class="number count-to" data-from="0" data-to="243" data-speed="1000" data-fresh-interval="20"><?php echo $this->m_guru->totalJamMengajar($this->m_guru->idu()) ?> Jam</div>
-                 </div>
-             </div>
-         </div>
-     </div>
- </div>
+											</ul>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-xl-4 col-lg-6 col-md-6 col-sm-12">
+							<div class="card  overflow-hidden project-card">
+								<div class="card-body">
+									<div class="d-flex">
+										<div class="my-auto">
+											<svg enable-background="new 0 0 438.891 438.891" class="mr-4 ht-60 wd-60 my-auto danger" version="1.1" viewBox="0 0 438.89 438.89" xml:space="preserve" xmlns="http://www.w3.org/2000/svg">
+											<path d="m374.1 385.52c71.682-74.715 69.224-193.39-5.492-265.08-34.974-33.554-81.584-52.26-130.05-52.193-103.54-0.144-187.59 83.676-187.74 187.22-0.067 48.467 18.639 95.077 52.193 130.05l-48.777 65.024c-5.655 7.541-4.127 18.238 3.413 23.893s18.238 4.127 23.893-3.413l47.275-63.044c65.4 47.651 154.08 47.651 219.48 0l47.275 63.044c5.655 7.541 16.353 9.069 23.893 3.413 7.541-5.655 9.069-16.353 3.413-23.893l-48.775-65.024zm-135.54 24.064c-84.792-0.094-153.51-68.808-153.6-153.6 0-84.831 68.769-153.6 153.6-153.6s153.6 68.769 153.6 153.6-68.769 153.6-153.6 153.6z"/>
+												<path d="m145.29 24.984c-33.742-32.902-87.767-32.221-120.67 1.521-32.314 33.139-32.318 85.997-8e-3 119.14 6.665 6.663 17.468 6.663 24.132 0l96.546-96.529c6.663-6.665 6.663-17.468 0-24.133zm-106.55 82.398c-12.186-25.516-1.38-56.08 24.136-68.267 13.955-6.665 30.175-6.665 44.131 0l-68.267 68.267z"/>
+												<path d="m452.49 24.984c-33.323-33.313-87.339-33.313-120.66 0-6.663 6.665-6.663 17.468 0 24.132l96.529 96.529c6.665 6.663 17.468 6.663 24.132 0 33.313-33.322 33.313-87.338 0-120.66zm-14.08 82.449-68.301-68.301c19.632-9.021 42.79-5.041 58.283 10.018 15.356 15.341 19.371 38.696 10.018 58.283z"/>
+												<path d="m238.56 136.52c-9.426 0-17.067 7.641-17.067 17.067v96.717l-47.787 63.71c-5.655 7.541-4.127 18.238 3.413 23.893s18.238 4.127 23.893-3.413l51.2-68.267c2.216-2.954 3.413-6.547 3.413-10.24v-102.4c1e-3 -9.426-7.64-17.067-17.065-17.067z"/>
+                                            </svg>
+										</div>
+										<div class="project-content">
+											<h5>JAM MENGAJAR</h5>
+											<ul>
+												<li>
+                                               <h3 class=" text-primary"><?php echo $this->m_guru->totalJamMengajarPerMinggu($this->m_guru->idu()) ?> Jam / Minggu</h3>
+													
+												</li>
+
+											</ul>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-xl-4 col-lg-6 col-md-6 col-sm-12">
+							<div class="card overflow-hidden project-card">
+								<div class="card-body">
+									<div class="d-flex">
+										<div class="my-auto">
+											<svg enable-background="new 0 0 477.849 477.849" class="mr-4 ht-60 wd-60 my-auto success" version="1.1" viewBox="0 0 477.85 477.85" xml:space="preserve" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="m374.1 385.52c71.682-74.715 69.224-193.39-5.492-265.08-34.974-33.554-81.584-52.26-130.05-52.193-103.54-0.144-187.59 83.676-187.74 187.22-0.067 48.467 18.639 95.077 52.193 130.05l-48.777 65.024c-5.655 7.541-4.127 18.238 3.413 23.893s18.238 4.127 23.893-3.413l47.275-63.044c65.4 47.651 154.08 47.651 219.48 0l47.275 63.044c5.655 7.541 16.353 9.069 23.893 3.413 7.541-5.655 9.069-16.353 3.413-23.893l-48.775-65.024zm-135.54 24.064c-84.792-0.094-153.51-68.808-153.6-153.6 0-84.831 68.769-153.6 153.6-153.6s153.6 68.769 153.6 153.6-68.769 153.6-153.6 153.6z"/>
+												<path d="m145.29 24.984c-33.742-32.902-87.767-32.221-120.67 1.521-32.314 33.139-32.318 85.997-8e-3 119.14 6.665 6.663 17.468 6.663 24.132 0l96.546-96.529c6.663-6.665 6.663-17.468 0-24.133zm-106.55 82.398c-12.186-25.516-1.38-56.08 24.136-68.267 13.955-6.665 30.175-6.665 44.131 0l-68.267 68.267z"/>
+												<path d="m452.49 24.984c-33.323-33.313-87.339-33.313-120.66 0-6.663 6.665-6.663 17.468 0 24.132l96.529 96.529c6.665 6.663 17.468 6.663 24.132 0 33.313-33.322 33.313-87.338 0-120.66zm-14.08 82.449-68.301-68.301c19.632-9.021 42.79-5.041 58.283 10.018 15.356 15.341 19.371 38.696 10.018 58.283z"/>
+												<path d="m238.56 136.52c-9.426 0-17.067 7.641-17.067 17.067v96.717l-47.787 63.71c-5.655 7.541-4.127 18.238 3.413 23.893s18.238 4.127 23.893-3.413l51.2-68.267c2.216-2.954 3.413-6.547 3.413-10.24v-102.4c1e-3 -9.426-7.64-17.067-17.065-17.067z"/>
+                                            </svg>
+										</div>
+										<div class="project-content">
+											<h6>TOTAL JAM MENGAJAR</h6>
+											<ul>
+												<li>
+                                                <h3 class=" text-primary"> <?php echo $this->m_guru->totalJamMengajar($this->m_guru->idu()) ?> Jam</h3>
+												</li>
+											</ul>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+
+                        <div class="row row-sm main-content-app mb-4">
+					<div class="col-xl-4 col-lg-5">
+						<div class="card">
+							<div class="main-content-left main-content-left-chat">
+								<nav class="nav main-nav-line main-nav-line-chat">
+									<a class="nav-link active" data-toggle="tab" href="">Pengumuman</a> <a class="nav-link" data-toggle="tab" href="">Pengaduan</a> <a class="nav-link" data-toggle="tab" href="">Info</a>
+								</nav>
+							
+								<div class="main-chat-list" id="ChatList">
+									<div class="media new">
+										<div class="main-img-user online">
+											<img alt="" src="https://laravel.spruko.com/azira/horizontal_light/assets/img/faces/5.jpg"> <span>2</span>
+										</div>
+										<div class="media-body">
+											<div class="media-contact-name">
+												<span>Socrates Itumay</span> <span>2 hours</span>
+											</div>
+											<p>Assalamualaikum wr.wb, Mulai hari ini,silahkan ibu/bapak guru untuk memberikan materi/tugas, dengan pembelajaran online, disesuaikan dengan jadwal masing2, semoga kita dalam keadaan sehat walafiat, aamiin,terima kasih</p>
+										</div>
+									</div>
+									<div class="media new">
+										<div class="main-img-user">
+											<img alt="" src="https://laravel.spruko.com/azira/horizontal_light/assets/img/faces/6.jpg"> <span>1</span>
+										</div>
+										<div class="media-body">
+											<div class="media-contact-name">
+												<span>Dexter dela Cruz</span> <span>3 hours</span>
+											</div>
+											<p>Maec enas tempus, tellus eget con dime ntum rhoncus, sem quam</p>
+										</div>
+									</div>
+									<div class="media selected">
+										<div class="main-img-user online"><img alt="" src="https://laravel.spruko.com/azira/horizontal_light/assets/img/faces/9.jpg"></div>
+										<div class="media-body">
+											<div class="media-contact-name">
+												<span>Reynante Labares</span> <span>10 hours</span>
+											</div>
+											<p>Nam quam nunc, bl ndit vel aecenas et ante tincid</p>
+										</div>
+									</div>
+									
+									<div class="media new">
+										<div class="main-img-user">
+											<img alt="" src="https://laravel.spruko.com/azira/horizontal_light/assets/img/faces/7.jpg"> <span>1</span>
+										</div>
+										<div class="media-body">
+											<div class="media-contact-name">
+												<span>Ariana Monino</span> <span>3 days</span>
+											</div>
+											<p>Maece nas tempus, tellus eget cond imentum rhoncus, sem quam</p>
+										</div>
+									</div>
+									
+									
+									
+									<div class="media border-bottom-0">
+										<div class="main-img-user"><img alt="" src="https://laravel.spruko.com/azira/horizontal_light/assets/img/faces/6.jpg"></div>
+										<div class="media-body">
+											<div class="media-contact-name">
+												<span>Samuel Lerin</span> <span>7 days</span>
+											</div>
+											<p>Nam quam nunc, blandit vel aecenas et ante tincid</p>
+										</div>
+									</div>
+								</div><!-- main-chat-list -->
+							</div>
+						</div>
+					</div>
 
 
  <div class="main-content-body">
-
-
-
      <div class="body">
          <?php
             $idu = $this->m_guru->idu();
@@ -161,8 +272,6 @@
 
              </span>
              <div class="progress">
-
-
                  <div class="progress-bar <?php echo $color; ?> progress-bar-striped active" role="progressbar" aria-valuenow="78" aria-valuemin="0" aria-valuemax="100" style="width: <?php echo $persentase; ?>%">
                      <?php echo $persentase; ?> %
                  </div>
@@ -170,6 +279,7 @@
          <?php } ?>
      </div>
 
+<BR>
 
      <div class="row clearfix">
 
